496d8cd4fcbdf31625b3def891ea2a4044cdf461
xeusteerapat <xeus085@gmail.com>
2021-06-02 23:07:57 +0700

    [INIT] init repo

---
+++ index.js
@@ -0,0 +1,13 @@
+const genRandomId = () => {
+  let id = '';
+
+  for (let i = 0; i < 4; i++) {
+    const crypto = require('crypto');
+    const randomB64String = crypto.randomBytes(4).toString('hex');
+    id += `${randomB64String}-`;
+  }
+
+  return id.slice(0, -1);
+};
+
+module.exports = genRandomId;
